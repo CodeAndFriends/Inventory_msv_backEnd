4a39d180bd1f9229233dd008f24fec3882ba12ed
Joaquin Druetta <31075448+joacodru@users.noreply.github.com>
2022-11-11 18:08:15 -0300

    CRUD completed (#1)
    Co-authored-by: Joaquin Druetta <joaquin.druetta@keylab.com.ar>

--- inventory/main.py
+++ inventory/main.py
@@ -1,5 +1,6 @@
 from typing   import Union
-from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
 from fastapi  import FastAPI
 
 app = FastAPI()
@@ -28,10 +29,28 @@ class Product(HashModel):
 
 @app.get('/products')
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
 
 
 @app.post('/products')
 def create(product: Product):
     return product.save()
 
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    product = Product.get(pk)
+    return product.delete(pk)
